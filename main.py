8640b3f56236f184717838f2d17bb9a8e3859853
Kenneth <kenneth.factor@edu.sait.ca>
2023-10-19 17:34:14 -0600

    Read and create file

--- main.py
+++ main.py
@@ -10,4 +10,15 @@ def count_consonant(info,file):
             else:
                 count_dict[char] += 1 #if not empty plus 1
     
+    write_data = file.write(count_dict) #Write on that file
+    return write_data
     
+    
+
+with open("./sample.ini", "r") as file:
+        data = file.read()
+new_file = open(f"count.txt", "w") #Creates a files
+
+output = count_consonant(data,new_file)
+
+print(output)
